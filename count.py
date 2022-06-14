f10cbfe2799a89b7f314c12cbc8cf6483d6e3242
iharsakhno <ihar.sakhno@gmail.com>
2022-06-15 00:06:37 +0300

    new

---
+++ count.py
@@ -0,0 +1,15 @@
+data = input("Date")
+f_data = data.split(".")
+print(f_data)
+print(type(f_data))
+
+for i in f_data:
+    i = day
+
+
+if n in ()
+
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    return '02' = 29
+else:
+    return '02' = 28
